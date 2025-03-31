996a2ad84eb639062a55be34751395d923f8cd38
John Manhart <manhartjohn@gmail.com>
2025-03-30 20:12:01 -0700

    Adding in the battle plan - 🎉 Victory

---
+++ src/config/spotify.template.js
@@ -0,0 +1,7 @@
+// Spotify API configuration
+export const SPOTIFY_CONFIG = {
+  clientId: "YOUR_SPOTIFY_CLIENT_ID",
+  clientSecret: "YOUR_SPOTIFY_CLIENT_SECRET",
+  redirectUri: "http://localhost:3000",
+  scopes: ["user-read-currently-playing", "user-read-recently-played"],
+};
